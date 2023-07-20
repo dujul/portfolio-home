1360f383b556c9e41ce8215226a4451a9ca6d556
Juna <junaverse@yahoo.com>
2023-07-20 16:47:39 +0200

    Added icons

---
+++ src/components/icons/FlutterIcon.tsx
@@ -0,0 +1,15 @@
+import "./FlutterIcon.css"
+
+export default function FlutterIcon(props: { className?: string }) {
+    return <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px" y="0px" viewBox="0 0 256 317" xmlSpace="preserve" className={props.className}>
+        <g><polygon className="st0" points="157.67,0 0,157.67 48.8,206.47 255.27,0"/>
+            <polygon className="st0" points="156.57,145.4 72.15,229.82 121.13,279.53 169.84,230.82 255.27,145.4"/>
+            <polygon className="st2" points="121.13,279.53 158.21,316.61 255.27,316.61 169.84,230.82"/>
+            <polygon className="st3" points="71.6,230.36 120.4,181.56 169.84,230.82 121.13,279.53"/>
+            <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="-173.6875" y1="548.7979" x2="-172.968"
+                y2="548.5386" gradientTransform="matrix(44.5891 0 0 -44.5901 7866.5254 24720.502)"><stop offset="0"/>
+                <stop offset="1"/></linearGradient>
+            <polygon className="st1" points="121.13,279.53 161.69,266.07 165.72,234.94"/></g>
+    </svg>
+}
--- src/model/FrameworkData.tsx
+++ src/model/FrameworkData.tsx
@@ -1,4 +1,11 @@
 import ReactIcon from "~/components/icons/ReactIcon";
+import TailwindIcon from "~/components/icons/TailwindIcon";
+import GitIcon from "~/components/icons/GitIcon";
+import SpringIcon from "~/components/icons/SpringIcon";
+import NextJsIcon from "~/components/icons/NextjsIcon";
+import AndroidIcon from "~/components/icons/AndroidIcon";
+import FlutterIcon from "~/components/icons/FlutterIcon";
+import LatexIcon from "~/components/icons/LatexIcon";
 
 export type FrameworkData = {
     icon: JSX.Element,
@@ -12,14 +19,49 @@ export const FRAMEWORKS: FrameworkData[] = [
         name: "React",
         icon: <ReactIcon className="h-[40px] w-[40px]"/>,
     },
+    {
+        id: "tailwind",
+        name: "Tailwind",
+        icon: <TailwindIcon className="h-[40px] w-[40px]"/>,
+    },
+    {
+        id: "git",
+        name: "Git",
+        icon: <GitIcon className="h-[40px] w-[40px]"/>,
+    },
+    {
+        id: "spring",
+        name: "Spring",
+        icon: <SpringIcon className="h-[40px] w-[40px]"/>,
+    },
+    {
+        id: "next",
+        name: "Next.js",
+        icon: <NextJsIcon className="h-[40px] w-[40px]"/>,
+    },
+    {
+        id: "android",
+        name: "Android",
+        icon: <AndroidIcon className="h-[40px] w-[40px]"/>,
+    },
+    {
+        id: "flutter",
+        name: "Flutter",
+        icon: <FlutterIcon className="h-[40px] w-[40px]"/>,
+    },
+    {
+        id: "latex",
+        name: "Latex",
+        icon: <LatexIcon className="h-[40px] w-[40px]"/>,
+    },
 ]
 
-for (let i = 0; i < 11; i++) {
-    FRAMEWORKS.push(
-        {
-            id: "react-" + i,
-            name: "React",
-            icon: <ReactIcon className="h-[40px] w-[40px]"/>
-        }
-    )
-}
+// for (let i = 0; i < 11; i++) {
+//     FRAMEWORKS.push(
+//         {
+//             id: "react-" + i,
+//             name: "React",
+//             icon: <ReactIcon className="h-[40px] w-[40px]"/>
+//         }
+//     )
+// }
