cdc7dad6505da96996c23d14fd3a6b119553ddeb
Juna <junaverse@yahoo.com>
2024-03-16 15:38:39 +0100

    Updated card design

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,25 +8,32 @@ type CardProps = {
 }
 
 export default function Card({data, addTag}: CardProps) {
-    return <div className={"card-wrapper w-full max-w-[400px] h-[210px] select-none m-1 lg:m-[1.5rem] relative"}>
-        <div className={"card w-full h-full bg-link shadow rounded-lg"}>
-            <img src={data.image} alt={"image"} className={"absolute w-full h-full object-cover"}/>
-            <div className={"content"}>
-                <div
-                    className={"relative flex flex-col w-full h-full bg-gradient-to-b from-slate-900/70 to-transparent text-white"}>
-                    <h3 className={"px-4 py-2"}>{data.name}</h3>
-                    <div className={"flex-1"}></div>
+    return <div className={"card-wrapper w-full max-w-[400px] h-[310px] select-none m-1 lg:m-[1.5rem] relative"}>
+        <div className={"bg-gradient-to-b from-gradient-start to-gradient-end h-full w-full rounded-lg p-1"}>
+            <a href={data.link} target={"_blank"} className={"card flex flex-col w-full h-full bg-bg-page rounded-lg p-2 "}>
+                <CardImage data={data}/>
+                <div className={"card-body flex-1 text-primary flex justify-center items-center text-lg font-bold bg-title-color bg-opacity-10 rounded-b-lg"}>
+                    {data.name}
                 </div>
-            </div>
-            <div className={"overlay"}>
-                <p className={"description"}>{data.descr}</p>
-            </div>
+            </a>
         </div>
+        <Toolbar data={data} addTag={addTag}/>
+    </div>
+}
 
-        <div className={"card-toolbar py-2 top-[80%] z-50 flex w-full flex-wrap justify-center absolute"}>
-            <div className={"flex justify-center bg-bg-nav hover:bg-opacity-80 transition-opacity duration-150 rounded-[15px] p-1"}>
-                {data.tags.map((tag) => <ClickableCapsule onClick={() => addTag?.(tag)} key={tag} tag={tag} mini={true}/>)}
-            </div>
+function CardImage({data}: CardProps) {
+    return <div className={"card-image w-full h-[230px] rounded-t-lg"}>
+        <img src={data.image} alt={"image"} className={"absolute w-full h-full object-cover"}/>
+        <div className={"overlay"}>
+            <p className={"description"}>{data.descr}</p>
+        </div>
+    </div>
+}
+
+function Toolbar({data, addTag}: CardProps) {
+    return <div className={"py-2 top-[-30px] z-50 flex w-full flex-wrap justify-center absolute"}>
+        <div className={" card-toolbar flex justify-center bg-bg-nav hover:bg-opacity-80 transition-opacity duration-150 rounded-[15px] p-1"}>
+            {data.tags.map((tag) => <ClickableCapsule onClick={() => addTag?.(tag)} key={tag} tag={tag} mini={true}/>)}
         </div>
     </div>
 }
