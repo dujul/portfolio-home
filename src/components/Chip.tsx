a487126fd1e7fb517697d46e867be51093b5ff7d
Juna <junaverse@yahoo.com>
2024-04-16 21:54:30 +0200

    Fixed layering

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -7,7 +7,7 @@ export default function Chip(props: ChipProps) {
     return <div  className={"m-3 p-px rounded-lg bg-gradient-to-b from-gradient-start to-gradient-end lg:m-7 drop-shadow-lg hover:shadow-chip hover:shadow-md"}>
         <div className={"bg-bg-page rounded-lg"}>
             <div
-                className="flex w-40 h-16 justify-between rounded-lg bg-title-color bg-opacity-10 backdrop-blur-[1px] items-center p-2 ">
+                className="flex w-40 h-16 justify-between rounded-lg bg-title-color bg-opacity-10 items-center p-2 ">
                 {props.icon}
                 <h3 className="pl-1.5 text-secondary flex-1 text-center">{props.name}</h3>
             </div>
