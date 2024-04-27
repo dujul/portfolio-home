5c1ea0bb9aa005dc7a904258f71caf3329b894e1
Juna <junaverse@yahoo.com>
2024-04-27 13:10:05 +0200

    Added base path pipeline abstraction

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
+const isProd = process.env.NODE_ENV === "production"
+
+function getBasePath() {
+    let basePath = "";
+
+    if (isProd && process.env.BASE_PATH){
+        if (process.env.BASE_PATH.startsWith("/") ){
+            basePath = process.env.BASE_PATH;
+        } else {
+            basePath = "/" + process.env.BASE_PATH;
+        }
+    }
+
+    console.log("getBasePath() : isProd = " + isProd);
+    console.log("getBasePath() : basePath = " + basePath);
+
+    return basePath
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: getBasePath(),
     images: {
-        unoptimized: true
+        unoptimized: true,
     }
 }
 
